6aefec2296cd8ff4b678085d790abfe4ee8e777a
jawherbalti <jawher-balti@live.com>
2023-02-12 16:43:17 +0100

    send unban requests

--- components/Banned.js
+++ components/Banned.js
@@ -5,52 +5,151 @@ import {
   Dimensions,
   Image,
   TouchableOpacity,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
 } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import { Ionicons } from "@expo/vector-icons";
+import { useState } from "react";
+import { auth, db } from "../firebase";
+import {
+  addDoc,
+  collection,
+  doc,
+  getDoc,
+  serverTimestamp,
+  updateDoc,
+} from "firebase/firestore";
 
 const Banned = () => {
-  const sendRequest = () => {};
+  const [input, setInput] = useState("");
+  const [unbanRequestSent, setUnbanRequestSent] = useState(false);
+
+  useEffect(() => {
+    if (auth.currentUser) {
+      getUser();
+    }
+  }, []);
+
+  const getUser = async () => {
+    const userRef = doc(db, "users", auth?.currentUser?.uid);
+    const userSnap = await getDoc(userRef);
+    setUnbanRequestSent(userSnap.data().unbanRequestSent);
+  };
+
+  const sendRequest = () => {
+    if (input !== "") {
+      const unbanRequest = {
+        id: auth?.currentUser?.uid,
+        displayName: auth?.currentUser?.displayName,
+        photoUrl: auth?.currentUser?.photoURL,
+        request: input,
+        timestamp: serverTimestamp(),
+      };
+
+      addDoc(collection(db, "requests"), unbanRequest);
+      updateDoc(doc(db, "users", auth?.currentUser?.uid), {
+        unbanRequestSent: true,
+      });
+      setUnbanRequestSent(true);
+    }
+    setInput("");
+  };
 
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView behavior="position" style={styles.container}>
       <Text style={styles.title}>
         This account has been banned! You can send an unban request to our
         moderator.
       </Text>
-      <Image
-        style={{
-          width: 250,
-          height: 250,
-        }}
-        source={require("../assets/banned.gif")}
-      />
-      <TouchableOpacity style={styles.button} onPress={sendRequest}>
-        <Ionicons name="warning" size={20} color="#001e2b" />
-        <Text style={styles.btnText}>Unban request</Text>
-      </TouchableOpacity>
-    </View>
+      <View>
+        <View style={styles.form}>
+          <Image
+            style={{
+              width: 250,
+              height: 250,
+            }}
+            source={require("../assets/banned.gif")}
+          />
+
+          {unbanRequestSent ? (
+            <Text style={{ color: "#fff" }}>
+              Your unban request has been sent
+            </Text>
+          ) : (
+            <>
+              <View style={styles.input}>
+                <Ionicons
+                  name="chatbubbles-outline"
+                  size={20}
+                  color="#001e2b"
+                />
+
+                <TextInput
+                  style={styles.textInput}
+                  placeholder="Why you should be unbanned?"
+                  value={input}
+                  onChangeText={(text) => setInput(text)}
+                  onSubmitEditing={sendRequest}
+                />
+              </View>
+              <TouchableOpacity style={styles.button} onPress={sendRequest}>
+                <Ionicons name="warning" size={20} color="#001e2b" />
+                <Text style={styles.btnText}>Unban request</Text>
+              </TouchableOpacity>
+            </>
+          )}
+        </View>
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    height: Dimensions.get("window").height,
     alignItems: "center",
     backgroundColor: "#001E2B",
+    flex: 1,
   },
   title: {
     textAlign: "center",
     color: "#ffffff",
     fontWeight: "bold",
     fontSize: 20,
-    marginTop: 50,
+    marginTop: 30,
+  },
+  form: {
+    alignItems: "center",
+    width: "100%",
+    padding: 15,
+  },
+  input: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#ececec",
+    bottom: 0,
+    height: 45,
+    padding: 10,
+    borderRadius: 10,
+    width: "80%",
+    marginBottom: 10,
+  },
+  textInput: {
+    color: "#001e2b",
+    marginLeft: 5,
+    width: "80%",
+    height: 45,
+    fontSize: 12,
   },
   button: {
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
-    width: "80%",
+    width: "45%",
+    height: 45,
     padding: 10,
     backgroundColor: "#00ed64",
     borderColor: "#ffffff",
